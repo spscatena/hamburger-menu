cb606a6bdf2bded69b899b990d121537f2a9edb3
Sabrina Scatena <sabrina.scatena@gmail.com>
2021-06-29 21:27:27 -0400

    nav work

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,17 @@
+document.addEventListener("click", clickHandlers);
+
+function clickHandlers(event) {
+  console.log(event.target);
+  if (event.target.matches("#pull")) {
+    document.querySelector("body").classList.toggle("show-nav");
+    event.preventDefault();
+  }
+}
+
 // store the link plus the API key in a variable
 const key = "uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0";
 const API = `https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=${key}`;
+const storagePrefix = "nyt-autosave";
 
 console.log(API);
 
@@ -27,8 +38,16 @@ function showData(stories) {
     .join("");
 
   document.querySelector(".stories").innerHTML = looped;
+  sessionStorage.setItem(storagePrefix, looped);
 }
 
 if (document.querySelector(".home")) {
-  getStories();
+  let saved = sessionStorage.getItem(storagePrefix);
+  if (saved) {
+    console.log("loading from session storage");
+    document.querySelector(".stories").innerHTML = saved;
+  } else {
+    console.log("loading from nytimes api");
+    getStories();
+  }
 }
